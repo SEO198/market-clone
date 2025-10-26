a192d8a3b58bd0784719d2f732d3d9bb8d427ee4
SEO198 <wish0725@naver.com>
2025-10-26 17:25:56 +0900

    🎇 글쓰기 기능 구현
    - 프론트엔드 글쓰기 페이지 form으로 작업했습니다.
    - 백엔드와 SQLite를 연동해서 이미지 및 데이터를 업로드하는 기능을 구현했습니다.

---
+++ frontend/write.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("write-form");
+const handleSubmitForm = async (event) => {
+  event.preventDefault();
+  const body = new FormData(form);
+  body.append("insertAt", new Date().getTime());
+
+  try {
+    const res = await fetch("/items", {
+      method: "POST",
+      body,
+    });
+    const data = await res.json();
+
+    if (data === "200") {
+      alert("등록에 성공했습니다!");
+      window.location.pathname = "/";
+    }
+  } catch (e) {
+    console.error("이미지 업로드에 실패했습니다.");
+  }
+};
+
+form.addEventListener("submit", handleSubmitForm);
