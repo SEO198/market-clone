4f1c8ea2112cbe48d3cb63815fffffb67a2b447d
SEO198 <wish0725@naver.com>
2025-10-27 22:03:37 +0900

    svelte, vite 로 바꾼 후 firebase deploy 세팅

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Router from "svelte-spa-router";
+  import Login from "./pages/Login.svelte";
+  import Main from "./pages/Main.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Write from "./pages/Write.svelte";
+  import NotFound from "./pages/NotFound.svelte";
+  import "./css/style.css";
+
+  const routes = {
+    "/": Main,
+    "/Login": Login,
+    "/Signup": Signup,
+    "/Write": Write,
+    "*": NotFound,
+  };
+</script>
+
+<Router {routes}></Router>
+<main></main>
+
+<style>
+</style>
